babd0b77cb99d9a181a33ecda6903709a15f15e7
MohammedAlDahleh <mohammedd993@gmail.com>
2022-04-18 02:06:40 +0300

    update the code js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,35 @@
 ' use strict ' ;
 var name = prompt  ("Please enter his/her name ?");
-var gender =  prompt ("Please enter you are his/her ?");
+console.log("Your name :" + name);
+var gender =  prompt ("Please enter your gender his/her ?");
+console.log("Your gender : " + gender);
 var age;
-if (gender.toLowerCase === "his")
+if (gender === "his")
 {
     console.log ("Your gender is Male" );
     
-}else if (gender.toLowerCase === "her")
+}else if (gender === "her")
 {
     console.log("Your gender is Female");
 }else{
     console.log("Undefined");
 }
-age = prompt ("Please enter his/her your age ?")
+age = prompt (`Please enter ${gender} your age ?`)
+console.log("Your Age :" + age);
 
 if (age <= 0)
 {
-    console.log(`Your age is : ${age}`);
+    
     alert(`Your age is : ${age}`);
     
 }
 confirm("Do you want to Skip welcome message?")
-if (gender.toLowerCase ==="his")
+if (gender === "his")
 {
 alert(`Welcome Mr ${name}`);
-}else if (gender.toLowerCase === "her")
+}else if (gender === "her")
 {
     alert(`Welcome Ms ${name}`);
 }else{
     alert("Welcome")
 }
-
-
